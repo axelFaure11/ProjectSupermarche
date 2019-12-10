d1b08355333f99d5c5e38f8a705c5233c1d8ec60
AxelFaure05 <akatsumero@outlook.fr>
2019-12-10 18:30:38 +0100

    - Trying to fix heroku not finding javafx.util

--- src/main/java/servlets/showPanier.java
+++ src/main/java/servlets/showPanier.java
@@ -16,8 +16,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import modele.ElementPanier;
-import modele.Panier;
 import modele.Produit;
 import modele.tempPanier;
 
